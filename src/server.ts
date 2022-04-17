ce329963911af6829cafe43629db46c0b6430cc8
abderrahmane <abderrahmaneosmani2@gmail.com>
2022-04-17 08:22:38 -0400

    init project

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import http from "http";
+import express, { Express } from "express";
+import morgan from "morgan";
+const router: Express = express();
+
+const httpServer = http.createServer(router);
+const PORT: any = process.env.PORT ?? 3000;
+router.use(morgan("dev"));
+
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
+
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "origin, X-Requested-With,Content-Type,Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+/** Routes */
+router.use("/", (req: any, res: any) => {
+  res.send("hello");
+});
+
+router.use((req, res, next) => {
+  const error = new Error("not found");
+  return res.status(404).json({
+    message: error.message,
+  });
+});
+
+httpServer.listen(PORT, () =>
+  console.log(`The server is running on port ${PORT}`)
+);
